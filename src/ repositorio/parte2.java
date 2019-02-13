5ad5540472ba952712aa262c5b68bf7364e96607
AlejandroMontelongoTrejo <47596978+AlejandroMontelongoTrejo@users.noreply.github.com>
2019-02-13 12:40:44 +0000

    Create parte2.java

---
+++ src/ repositorio/parte2.java
@@ -0,0 +1,18 @@
+paquete  actividad3 ;
+ clase  pública Actividad3 {
+
+     int privado ;
+     int int privado ;
+    
+    Actividad3 ( int  a , int  b ) {
+        esta . a = a;
+        esta . b = b;
+    }
+    
+     suma doble  pública () {
+        doble resultado
+        resultado = a + b;
+        resultado de retorno
+    }
+    
+}
