ac189e65d0e20be7d3cef86689a3b6b281813fee
AlejandroMontelongoTrejo <47596978+AlejandroMontelongoTrejo@users.noreply.github.com>
2019-02-13 12:29:19 +0000

    Parte2
    Hice la parte 2

---
+++ actividad3.java
@@ -0,0 +1,18 @@
+package actividad3;
+public class Actividad3 {
+
+    private int a;
+    private int b;
+    
+    Actividad3(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+    
+    public double suma(){
+        double resultado;
+        resultado=a+b;
+        return resultado;
+    }
+    
+}
